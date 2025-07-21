b866988d691508ccdd0c94131d33f3cb3e5e42a2
Sinduja Ramanan <87081967+sinduja-r@users.noreply.github.com>
2025-07-21 13:33:20 +0100

    Create main.tf
    Security Groups have open ports

---
+++ open_security_group/main.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "open_sg" {
+  name        = "open-sg"
+  description = "Open to the world"
+  vpc_id      = "vpc-12345678"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
