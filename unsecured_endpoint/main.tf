174af4eea0e0e7a1a81dbf662c0555893959ea57
Sinduja Ramanan <87081967+sinduja-r@users.noreply.github.com>
2025-07-21 16:19:57 +0100

    Update main.tf

--- unsecured_endpoint/main.tf
+++ unsecured_endpoint/main.tf
@@ -1,7 +1,11 @@
 resource "aws_api_gateway_rest_api" "insecure_api" {
   name        = "InsecureAPI"
   description = "Public REST API without authentication"
+  tags = {
+    endpoint_url = "http://insecure.example.com/api"
+  }
 }
+
 
 resource "aws_api_gateway_deployment" "deploy" {
   rest_api_id = aws_api_gateway_rest_api.insecure_api.id
