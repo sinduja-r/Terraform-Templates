33ed4d838848b5c64eb58e49cb9cccf10398232d
Sinduja Ramanan <87081967+sinduja-r@users.noreply.github.com>
2025-07-21 13:32:01 +0100

    Create main.tf
    Policy with Allow All statements

---
+++ over_permissive_iam/main.tf
@@ -0,0 +1,14 @@
+resource "aws_iam_policy" "bad_policy" {
+  name        = "OverPermissivePolicy"
+  description = "Bad IAM policy with wildcard actions"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action   = "*"
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
