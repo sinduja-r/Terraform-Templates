f35d05f83653496c0356132c1c005d6cadb02b94
Sinduja Ramanan <87081967+sinduja-r@users.noreply.github.com>
2025-07-21 13:36:51 +0100

    Create main.tf
    Endpoints arent secured

---
+++ unsecured_endpoint/main.tf
@@ -0,0 +1,9 @@
+resource "aws_api_gateway_rest_api" "insecure_api" {
+  name        = "InsecureAPI"
+  description = "Public REST API without authentication"
+}
+
+resource "aws_api_gateway_deployment" "deploy" {
+  rest_api_id = aws_api_gateway_rest_api.insecure_api.id
+  stage_name  = "prod"
+}
