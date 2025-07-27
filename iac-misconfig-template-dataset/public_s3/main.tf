41375e8b1dd73bb0bcc9a4c577e39c021053eb2e
Sinduja Ramanan <87081967+sinduja-r@users.noreply.github.com>
2025-07-27 22:14:48 +0100

    Update main.tf

--- iac-misconfig-template-dataset/public_s3/main.tf
+++ iac-misconfig-template-dataset/public_s3/main.tf
@@ -1,9 +1,9 @@
-resource "aws_s3_bucket" "bad_bucket1" {
-  bucket = "insecure-public-bucket"
-  acl    = "public-read"
-}
+#resource "aws_s3_bucket" "bad_bucket1" {
+#  bucket = "insecure-public-bucket"
+#  acl    = "public-read"
+#}
 
-resource "aws_s3_bucket" "bad_bucket2" {
-  bucket = "public-bucket-3"
-  acl    = "public-read-write"
-}
+#resource "aws_s3_bucket" "bad_bucket2" {
+#  bucket = "public-bucket-3"
+#  acl    = "public-read-write"
+#}
