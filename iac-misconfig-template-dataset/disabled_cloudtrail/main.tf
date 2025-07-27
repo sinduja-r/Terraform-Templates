b1129c67b6002e01778cf34b736824b804f49568
Sinduja Ramanan <87081967+sinduja-r@users.noreply.github.com>
2025-07-27 22:11:40 +0100

    Update main.tf

--- iac-misconfig-template-dataset/disabled_cloudtrail/main.tf
+++ iac-misconfig-template-dataset/disabled_cloudtrail/main.tf
@@ -1,12 +1,12 @@
 # Simulates a deployment where CloudTrail is not configured at all
 # You can create an empty file or leave CloudTrail intentionally absent in this test case
 # OR simulate a misconfigured trail:
-"""
-resource "aws_cloudtrail" "disabled_trail" {
-  name                          = "disabled-trail"
-  s3_bucket_name                = "some-log-bucket"
-  include_global_service_events = false
-  is_multi_region_trail         = false
-  enable_logging                = false
-}
-"""
+
+#resource "aws_cloudtrail" "disabled_trail" {
+#  name                          = "disabled-trail"
+#  s3_bucket_name                = "some-log-bucket"
+#  include_global_service_events = false
+#  is_multi_region_trail         = false
+#  enable_logging                = false
+#}
+
