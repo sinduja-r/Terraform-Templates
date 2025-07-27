5783f0e949d12c16e2d4bf1806cf285441b25212
Sinduja Ramanan <87081967+sinduja-r@users.noreply.github.com>
2025-07-27 22:15:02 +0100

    Update main.tf

--- iac-misconfig-template-dataset/unencrypted_storage/main.tf
+++ iac-misconfig-template-dataset/unencrypted_storage/main.tf
@@ -1,5 +1,5 @@
-resource "aws_ebs_volume" "unencrypted_volume" {
-  availability_zone = "us-east-1a"
-  size              = 8
-  encrypted         = false
-}
+#resource "aws_ebs_volume" "unencrypted_volume" {
+#  availability_zone = "us-east-1a"
+#  size              = 8
+#  encrypted         = false
+#}
