e99b871ef1cbb7bfe68f194bb708938238c3109d
Sinduja Ramanan <87081967+sinduja-r@users.noreply.github.com>
2025-07-27 22:15:23 +0100

    Update main.tf

--- iac-misconfig-template-dataset/unsecured_endpoint/main.tf
+++ iac-misconfig-template-dataset/unsecured_endpoint/main.tf
@@ -1,13 +1,13 @@
-resource "aws_api_gateway_rest_api" "insecure_api" {
-  name        = "InsecureAPI"
-  description = "Public REST API without authentication"
-  tags = {
-    endpoint_url = "http://insecure.example.com/api"
-  }
-}
+#resource "aws_api_gateway_rest_api" "insecure_api" {
+#  name        = "InsecureAPI"
+#  description = "Public REST API without authentication"
+#  tags = {
+#    endpoint_url = "http://insecure.example.com/api"
+#  }
+#}
 
 
-resource "aws_api_gateway_deployment" "deploy" {
-  rest_api_id = aws_api_gateway_rest_api.insecure_api.id
-  stage_name  = "prod"
-}
+#resource "aws_api_gateway_deployment" "deploy" {
+#  rest_api_id = aws_api_gateway_rest_api.insecure_api.id
+#  stage_name  = "prod"
+#}
